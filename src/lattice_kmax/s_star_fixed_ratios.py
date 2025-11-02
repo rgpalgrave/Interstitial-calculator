0d5e2f7917703396ddfd1220598e47b66eba09f7
rgpalgrave <rgpalgrave@gmail.com>
2025-11-02 15:35:10 +0000

    Create s_star_fixed_ratios.py

---
+++ src/lattice_kmax/s_star_fixed_ratios.py
@@ -0,0 +1,5 @@
+# src/lattice_kmax/s_star_fixed_ratios.py
+# Thin re-export so you can import either:
+#   from lattice_kmax.sstar import s_star_fixed_ratios
+#   from lattice_kmax.s_star_fixed_ratios import s_star_fixed_ratios
+from .sstar import s_star_fixed_ratios
